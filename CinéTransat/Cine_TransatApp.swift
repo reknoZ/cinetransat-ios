7ed0f73b6e97e6e5e4d4624b4d7fea3b5c4379fb
David Nitzsche-Bell <david@zicount.com>
2026-04-18 11:01:12 +0200

    Initial Commit

---
+++ CinéTransat/Cine_TransatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cine_TransatApp.swift
+//  CinéTransat
+//
+//  Created by David on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Cine_TransatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
